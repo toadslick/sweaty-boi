70970eec81be477ce530e575362576acb36b93ef
Toadslick <to@dslick>
2019-06-25 07:24:38 -0700

    boop

--- src/index.js
+++ src/index.js
@@ -1,26 +1,35 @@
-import { distance } from './utils';
+import { distance, angle, average } from "./utils";
 
-const CURSOR_INTERVAL = 300;
-const MIN_ACTIVITY_DECAY = 50;
-const MAX_ACTIVITY_RATE = 5000;
-const ACTIVITY_DIVISOR = 15;
+const CURSOR_INTERVAL = 150;
+const MAX_ACTIVITY_RATE = 10000;
+const ACTIVITY_DECAY = 0.98;
 
 let prevCursorX = 0;
 let prevCursorY = 0;
-let totalDist = 0;
+let frameDistance = 0;
 let activityRate = 0;
+let frameAngles = [];
 
-const meterDiv = document.getElementById('rate-meter');
+const activityDiv = document.getElementById("activity-meter");
+const vectorDiv = document.getElementById("direction-indicator");
 
-document.addEventListener('mousemove', ({ screenX, screenY }) => {
-  totalDist += distance(prevCursorX, prevCursorY, screenX, screenY);
+document.addEventListener("mousemove", ({ screenX, screenY }) => {
+  const ang = angle(prevCursorX, prevCursorY, screenX, screenY);
+  angle && frameAngles.push(ang);
+  frameDistance += distance(prevCursorX, prevCursorY, screenX, screenY);
   prevCursorX = screenX;
   prevCursorY = screenY;
 });
 
 window.setInterval(() => {
-  const cooldown = Math.max(activityRate / ACTIVITY_DIVISOR, MIN_ACTIVITY_DECAY);
-  activityRate = Math.min(Math.max(activityRate + totalDist - cooldown, 0), MAX_ACTIVITY_RATE);
-  meterDiv.style.width = `${(activityRate / MAX_ACTIVITY_RATE) * 100}%`;
-  totalDist = 0;
+  activityRate = Math.min(
+    (activityRate + frameDistance) * ACTIVITY_DECAY,
+    MAX_ACTIVITY_RATE
+  );
+  activityDiv.style.width = `${(activityRate / MAX_ACTIVITY_RATE) * 100}%`;
+  vectorDiv.style.width = `${frameDistance * 0.75}px`;
+  vectorDiv.style.transform = `rotate(${average(frameAngles)}deg)`;
+
+  frameDistance = 0;
+  frameAngles = [];
 }, CURSOR_INTERVAL);
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,9 @@
-const distance = (x1, y1, x2, y2) => Math.abs(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5);
+const distance = (x1, y1, x2, y2) =>
+  Math.abs(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5);
 
-export { distance };
+const angle = (x1, y1, x2, y2) =>
+  360 - (180 / Math.PI) * Math.atan2(x1 - x2, y1 - y2);
+
+const average = numbers => numbers.reduce((a, b) => a + b, 0) / numbers.length;
+
+export { distance, angle, average };
