1e9643fef7b7a3d90755bfa120d6ba9c11cff8c8
Toadslick <to@dslick>
2019-06-25 00:43:59 -0700

    boop

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-import { distance, } from './utils';
+import { distance } from './utils';
 
-console.log(distance(3, 5));
+const CURSOR_INTERVAL = 300;
+const MIN_ACTIVITY_DECAY = 50;
+const MAX_ACTIVITY_RATE = 5000;
+const ACTIVITY_DIVISOR = 10;
+
+let prevCursorX = 0;
+let prevCursorY = 0;
+let totalDist = 0;
+let activityRate = 0;
+
+const meterDiv = document.getElementById('rate-meter');
+
+document.addEventListener('mousemove', ({ screenX, screenY }) => {
+  totalDist += distance(prevCursorX, prevCursorY, screenX, screenY);
+  prevCursorX = screenX;
+  prevCursorY = screenY;
+});
+
+window.setInterval(() => {
+  const cooldown = Math.max(activityRate / ACTIVITY_DIVISOR, MIN_ACTIVITY_DECAY);
+  activityRate = Math.min(Math.max(activityRate + totalDist - cooldown, 0), MAX_ACTIVITY_RATE);
+  meterDiv.style.width = `${(activityRate / MAX_ACTIVITY_RATE) * 100}%`;
+  totalDist = 0;
+}, CURSOR_INTERVAL);
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
-const distance = (x, y) => (x ** 2 + y ** 2) ** 0.5;
+const distance = (x1, y1, x2, y2) => Math.abs(((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5);
 
-export { distance, };
+export { distance };
