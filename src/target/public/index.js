d4f7012ca93e037e22c213336d312c897eb4fcef
James Childress <jchildress@cvent.com>
2019-06-28 13:39:44 -0700

    boop

--- src/target/public/index.js
+++ src/target/public/index.js
@@ -2,5 +2,18 @@ import Runner from "../../runner";
 
 import "./index.scss";
 
+const ALL_RADIOS_SELECTOR = "#sweatyboi-mode-select input";
+const CHECKED_RADIO_SELECTOR = "#sweatyboi-mode-select input:checked";
+const MAIN_CONTENT_SELECTOR = "#sweatyboi-main";
+
 const runner = new Runner();
-runner.mode("sweat");
+
+const onModeChanged = () => {
+  const radio = document.querySelector(CHECKED_RADIO_SELECTOR);
+  const main = document.querySelector(MAIN_CONTENT_SELECTOR);
+  runner.mode(radio.value);
+  main.scrollIntoView({ behavior: "smooth" });
+};
+
+const radios = document.querySelectorAll(ALL_RADIOS_SELECTOR);
+radios.forEach(radio => radio.addEventListener("change", onModeChanged));
