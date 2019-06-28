2098546babddfff3f9e0caf32660401db246ab58
James Childress <jchildress@cvent.com>
2019-06-28 03:45:00 -0700

    webpack multiple build targets

--- src/target/development/index.js
+++ src/target/development/index.js
@@ -1,6 +1,8 @@
 import Runner from "../../runner";
 import DevelopmentMonitor from "./monitor";
 
+import "./index.scss";
+
 const runner = new Runner(DevelopmentMonitor);
 
 const select = document.getElementById("mode-select");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
@@ -15,27 +16,37 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   },
   devtool: "source-map",
-  watch: true,
+  // watch: true,
   module: {
     rules: [
       {
-        test: /\.m?js$/,
+        test: /\.js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["transform-class-properties"]
+            presets: ["@babel/preset-env"]
           }
         }
+      },
+      {
+        test: /\.html$/,
+        use: "html-loader"
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"]
       }
     ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/target/development/index.html",
-      chunks: [],
-      filename: "development/index.html"
+      filename: "development/index.html",
+      chunks: []
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name]/index.css"
     })
   ]
 };
